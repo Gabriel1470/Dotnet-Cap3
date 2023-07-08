747feca539fb8434e4d236056ef321be3f83fba0
Gabriel1470 <gabriel1470bg@gmail.com>
2023-07-08 08:21:33 +0200

    Bho experiment?

--- Program.cs
+++ Program.cs
@@ -1,23 +1,8 @@
-﻿var randList = new Random();
-var doubleList = new List<double>();
+﻿var oneList = new List<string>() {"lista1-1"};
+var twoList = new List<string>() {"lista2-2", "lista3-3"};
 
-for  (int i = 0; i < 10; i++)
-{
-    doubleList.Add(randList.NextDouble() * 10);
-}
+Console.WriteLine(oneList.FirstOrDefault());
+Console.WriteLine(twoList.FirstOrDefault());
 
-Console.WriteLine($"Elementi numerici nella lista {doubleList.Count()}");
-Console.WriteLine("Digita un numero per Skip");
-int usInp1 = int.Parse(Console.ReadLine());
-Console.WriteLine("Digita un numero per Take");
-int usInp2 = int.Parse(Console.ReadLine());
-
-var skTkList = doubleList.Skip(usInp1).Take(usInp2);
-
-var somma = skTkList.Sum();
-var media = skTkList.Average();
-var conta = skTkList.Count();
-
-Console.WriteLine("somma di tutti gli elementi della lista: " + somma);
-Console.WriteLine("media della lista: " + media);
-Console.WriteLine("la lista ha " + conta + " elementi");
+Console.WriteLine(oneList.SingleOrDefault());
+Console.WriteLine(twoList.SingleOrDefault());
