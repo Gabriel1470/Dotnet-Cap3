d3a8b7295e895b8926d54cf30ff026d23a760f17
Gabriel1470 <gabriel1470bg@gmail.com>
2023-07-08 07:29:54 +0200

    e un casino ma forse funziona

--- Program.cs
+++ Program.cs
@@ -4,7 +4,26 @@ for (int i = 0; i < 1000; i++)
 {
     intList.Add(randList.Next(0, 1001));
 }
-var results = intList.Where(i => i % 2 == 1);
-   
-
+var results = intList.Where(integ => integ % 2 == 1);
 results.ToList().ForEach(result => Console.Write(result + " - "));
+
+var ordered =
+    from i in intList
+    orderby i
+    select i;
+    
+Console.WriteLine("-----------------------------------------------------\n");
+   Console.WriteLine("Ordered List \n");
+ordered.ToList().ForEach(x => Console.Write(x + " - "));
+Console.WriteLine();
+Console.WriteLine("------------------------------------------------------\n");
+
+var reversed =
+    from i in intList
+    orderby i descending
+    select i;
+
+    Console.WriteLine("Reversed List \n");
+    
+reversed.ToList().ForEach(x => Console.Write(x + " - "));
+Console.WriteLine();
