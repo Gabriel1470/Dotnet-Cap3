b1c207d493055699ba257f0f9fd75dd662d77f00
Gabriel1470 <gabriel1470bg@gmail.com>
2023-07-08 08:14:39 +0200

    Skip, Take, Sum it up non so

--- Program.cs
+++ Program.cs
@@ -3,12 +3,20 @@ var doubleList = new List<double>();
 
 for  (int i = 0; i < 10; i++)
 {
-    doubleList.Add(randList.NextDouble()*10);
+    doubleList.Add(randList.NextDouble() * 10);
 }
 
-var somma = doubleList.Sum();
-var media = doubleList.Average();
-var conta = doubleList.Count();
+Console.WriteLine($"Elementi numerici nella lista {doubleList.Count()}");
+Console.WriteLine("Digita un numero per Skip");
+int usInp1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Digita un numero per Take");
+int usInp2 = int.Parse(Console.ReadLine());
+
+var skTkList = doubleList.Skip(usInp1).Take(usInp2);
+
+var somma = skTkList.Sum();
+var media = skTkList.Average();
+var conta = skTkList.Count();
 
 Console.WriteLine("somma di tutti gli elementi della lista: " + somma);
 Console.WriteLine("media della lista: " + media);
