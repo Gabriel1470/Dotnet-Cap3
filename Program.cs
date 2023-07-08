86006d3224d9904bbcc30f785ebf998578147874
Gabriel1470 <gabriel1470bg@gmail.com>
2023-07-08 07:14:59 +0200

    queri list random number

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var randList = new Random();
+var intList = new List<int>();
+for (int i = 0; i < 1000; i++)
+{
+    intList.Add(randList.Next(0, 1001));
+}
+var results =
+    from integer in intList
+    where integer % 2 == 1
+    select integer;
+;
+results.ToList().ForEach(result => Console.Write(result + " - "));
