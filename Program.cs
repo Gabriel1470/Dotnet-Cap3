831506073330ede450da9b688f0b986a7adee83f
Gabriel1470 <gabriel1470bg@gmail.com>
2023-07-08 08:00:30 +0200

    Sorted ho fatto casino co commit e branche

--- Program.cs
+++ Program.cs
@@ -1,15 +1,29 @@
 ﻿var randList = new Random();
-var doubleList = new List<double>();
-
-for  (int i = 0; i < 10; i++)
+var intList = new List<int>();
+for (int i = 0; i < 1000; i++)
 {
-    doubleList.Add(randList.NextDouble()*10);
+    intList.Add(randList.Next(0, 1001));
 }
+var results = intList.Where(integ => integ % 2 == 1);
+results.ToList().ForEach(result => Console.Write(result + " - "));
 
-var somma = doubleList.Sum();
-var media = doubleList.Average();
-var conta = doubleList.Count();
+var ordered =
+    from i in intList
+    orderby i
+    select i;
+    
+Console.WriteLine("-----------------------------------------------------\n");
+   Console.WriteLine("Ordered List \n");
+ordered.ToList().ForEach(x => Console.Write(x + " - "));
+Console.WriteLine();
+Console.WriteLine("------------------------------------------------------\n");
 
-Console.WriteLine("somma di tutti gli elementi della lista: " + somma);
-Console.WriteLine("media della lista: " + media);
-Console.WriteLine("la lista ha " + conta + " elementi");
+var reversed =
+    from i in intList
+    orderby i descending
+    select i;
+
+    Console.WriteLine("Reversed List \n");
+    
+reversed.ToList().ForEach(x => Console.Write(x + " - "));
+Console.WriteLine();
