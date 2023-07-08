624c6a7b5143a6b35b6340dff04dc6e61b36eb98
Gabriel1470 <gabriel1470bg@gmail.com>
2023-07-08 07:20:06 +0200

    Clear

--- Program.cs
+++ Program.cs
@@ -4,9 +4,7 @@ for (int i = 0; i < 1000; i++)
 {
     intList.Add(randList.Next(0, 1001));
 }
-var results =
-    from integer in intList
-    where integer % 2 == 1
-    select integer;
-;
+var results = intList.Where(i => i % 2 == 1);
+   
+
 results.ToList().ForEach(result => Console.Write(result + " - "));
