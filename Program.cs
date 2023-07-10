29c296d1ad9c93d895e065ffe03dae9227acb4ec
Gabriel1470 <gabriel1470bg@gmail.com>
2023-07-10 06:06:33 +0200

    Join?

--- Program.cs
+++ Program.cs
@@ -1,15 +1,37 @@
-﻿var randList = new Random();
-var intList = new List<int>();
+﻿using Dotnet_Cap3;
 
-
-
-for (int i = 0; i < 10; i++)
+var products = new List<Product>
 {
-    intList.Add(randList.Next(0,10));
-}
+    new Product{Name="Milk", Price=1.2, CategoryId=1},
+    new Product{Name="Orange Juice", Price=3.2, CategoryId=3},
+    new Product{Name="Milka Biscuits", Price=3.2, CategoryId=2},
+    new Product{Name="Tea biscuits", Price=3.2, CategoryId=2},
+    new Product{Name="Coca Cola", Price=3.2, CategoryId=3},
+    new Product{Name="Parmigiano", Price=5.0, CategoryId=1},
+};
 
-var pari = intList.All(x => x % 2 == 0);
-var dispari = intList.Any(x => x % 2 == 1);
+var categories = new List<Category>
+{
+    new Category{Id=1, Name="Dairy"},
+    new Category{Id=2, Name="Biscuits"},
+    new Category{Id=3, Name="Drinks"}
+};
 
-Console.WriteLine($"Sono tutti  pari? {pari}");
-Console.WriteLine($"Ci sono numeri dispari? {dispari}");
+var results = products.Join(
+    categories,
+    product => product.CategoryId,
+    category => category.Id,
+    (product, category) => new
+    {
+        Name = product.Name,
+        Price = product.Price,
+        Category = category.Name
+    });
+
+foreach (var product in results)
+{
+    Console.WriteLine($"Name: {product.Name}");
+    Console.WriteLine($"Price: {product.Price}");
+    Console.WriteLine($"Category: {product.Category}");
+    Console.WriteLine();
+};
